fbdb53c6fd4d59773ac63b61626f8b9688e7d191
todai88 <joabaj88@gmail.com>
2017-07-15 16:03:47 +0100

    pr0n

---
+++ CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,45 @@
+﻿using CountingKs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using CountingKs.Data.Entities;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private ICountingKsRepository _repo;
+
+        public FoodsController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<object> Get()
+        {
+            var repo = new CountingKsRepository(
+                new CountingKsContext());
+
+            var results = repo.GetAllFoodsWithMeassures()
+                              .OrderBy(f => f.Description)
+                              .Take(25)
+                              .ToList()
+                              .Select(f => new
+                              {
+                                  Description = f.Description,
+                                  Measures = f.Measures
+                                              .Select(m => new
+                                              {
+                                                  Description = m.Description,
+                                                  Calories = m.Calories
+                                              })
+                              });
+
+            return results;
+        }
+    }
+}
